edb7a0d309aece9dad2c94ab46acdec2776f2372
JakeBrose <mordecaistudios@gmail.com>
2017-12-05 14:45:42 -0600

    logs correctly

--- chaining.js
+++ chaining.js
@@ -3,4 +3,10 @@ function sortNumber(a, b) {
 }
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 integers.sort(sortNumber);
+integers.splice(-14, 4);
 console.log(integers.join(','));
+for (let i = 0; i < integers.length; i++) {
+    intMod = (integers[i] * 1.5) - 1 
+    console.log(intMod); 
+}
+
